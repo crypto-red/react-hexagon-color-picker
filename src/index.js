28adcecce0f53ccb0e1cc2d731138aca8f6e1677
Viper <contact@vipertech.ch>
2021-07-31 19:19:05 +0200

    V 0.1.1

--- src/index.js
+++ src/index.js
@@ -1,7 +1,7 @@
 import HexagonalColorHue from "./components/HexagonalColorHue";
 import HexagonalColorPicker from "./components/HexagonalColorPicker";
 
-module.exports = {
-    HexagonalColorHue: HexagonalColorHue,
-    HexagonalColorPicker: HexagonalColorPicker,
+export {
+    HexagonalColorHue,
+    HexagonalColorPicker
 };
